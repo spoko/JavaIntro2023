96ace2dfa09503a0a2c9bf8b33cf38dc349dc353
Ibraim Asanov <iasanov@nemetschek.bg>
2023-03-09 21:04:48 +0200

    Initial commit

---
+++ src/main/java/qa/intro/MyFirstClass.java
@@ -0,0 +1,38 @@
+package qa.intro;
+
+import java.util.Scanner;
+
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+        int five = 7;
+
+        System.out.println("Hello World");
+
+        System.out.println(five);
+
+        //Reading input from the console
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name:");
+
+        String userName = scanner.nextLine();
+
+        System.out.println("Nice to meet you, " + userName + "!");
+
+        System.out.println("Please enter your age:");
+
+        int userAge = scanner.nextInt();
+
+        if (userAge < 0){
+            System.out.println("Invalid data!");
+            return;
+        }
+
+        if (userAge < 18){
+            System.out.println(userName + " is not allowed to drink!");
+        }else{
+            System.out.println("Cheers, " + userName);
+        }
+    }
+}
